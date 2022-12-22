73de36762880f7a626e406843b29bf26cfabaade
Renan Veronez Drechsler <renanvz@outlook.com>
2022-12-22 17:04:59 +0000

    Main Components

---
+++ frontend/src/components/Profile.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const Profile = () => {
+  //👇🏻 The ID is the URL parameter for fetching the user's details.
+  const { id } = useParams();
+
+  return (
+    <main className='profile'>
+      <div style={{ width: "70%" }}>
+        <h2>Hey, nevodavid</h2>
+        <p>Here is your schedule: WAT</p>
+        <table>
+          <tbody>
+            <tr>
+              <td>MON</td>
+              <td>8:00am</td>
+              <td>10:00pm</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </main>
+  );
+};
+
+export default Profile;
